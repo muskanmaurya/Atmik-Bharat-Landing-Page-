2a31bb3e6f1559dd3bd3d90c613b1019a48fe1d5
muskan maurya <muskanmaurya0987@gmail.com>
2026-06-17 23:06:40 +0530

    UI: implemented innovations section dummy layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         <HeroSection />
         <InnovationsSection/>
         <ServicesSection />
-        {/* <ServicesCarousel slides={servicesSlides} /> */}
+        <ServicesCarousel slides={servicesSlides} />
         {/* <InventivAISection /> */}
         <StatsMarquee />
         <TechStackSection />
