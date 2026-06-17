98dfb67516110af1aa6fc84e3e2bbca6e5b822ff
muskan maurya <muskanmaurya0987@gmail.com>
2026-06-17 23:11:23 +0530

    UI: implemented innovations section dummy layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,8 +48,8 @@ export default function Home() {
         {/* <MarketingCTA /> */}
         <FAQSection />
       </main>
-      {/* <Footer /> */}
-      {/* <ContactModal /> */}
+      <Footer />
+      <ContactModal />
     </SmoothScroll>
   );
 }
